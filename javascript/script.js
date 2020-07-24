1539085dc11334f9400215288fd3c0673eb50342
JordanBelmont <jordan.belmont@hotmail.com>
2020-07-24 13:37:48 +0100

    changed gallery.css to art-gallery.css, script.js created for modal functionality to the art gallery images

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+let lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+// creates a div called 'lightbox' in the body
+
+let images = document.querySelectorAll('.gallery-image-1, .gallery-image-2')
+// select all elements with certain classes
+images.forEach(image => {
+   image.addEventListener('click', e => {
+      lightbox.classList.add('active')
+      let img = document.createElement('img')
+      img.src = image.src
+      while (lightbox.firstChild) {
+         lightbox.removeChild(lightbox.firstChild)
+      }
+      lightbox.appendChild(img)
+   })
+})
+
+lightbox.addEventListener('click', e => {
+   if (e.target !== e.currentTarget) return
+   lightbox.classList.remove('active')
+})
