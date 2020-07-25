48da3df95996bdf0d84aebc88ad36efd7ff4666f
JordanBelmont <jordan.belmont@hotmail.com>
2020-07-25 16:10:11 +0100

    lightbox.css created, art-gallery.css moved into gallery.css. Swapped out flex layout for grid layouts in copy.html and art.html galleries. Swapped modal for lightbox effect and added text hover and brightness change to images

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 // creates a div called 'lightbox' in the body
 
-let images = document.querySelectorAll('.gallery-image-1, .gallery-image-2, .gallery-image-3')
+let images = document.querySelectorAll('.gallery-img')
 // select all elements with certain classes
 images.forEach(image => {
    image.addEventListener('click', e => {
