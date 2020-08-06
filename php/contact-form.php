e8907ad00f87bd950ffa4c701f206b4d9e15403b
JordanBelmont <jordan.belmont@hotmail.com>
2020-08-06 19:48:37 +0100

    contact-form.php created with form submission capabilities

---
+++ php/contact-form.php
@@ -0,0 +1,17 @@
+<?php
+   $name = $_POST['name'];
+   $visitor_email = $_POST['email'];
+   $message = $_POST['message'];
+
+   $email_from = 
+
+   $email_subject = "New Form Submission";
+
+   $email_body = "Contact Name: $name.\n".
+                  "Contact Email: $visitor_email.\n".
+                  "Message: $message.\n";
+
+
+
+
+?>
