a0dbc9a6b0896bc01d2284405f88a83ef9964114
JordanBelmont <jordan.belmont@hotmail.com>
2020-10-18 22:55:33 +0100

    added media query to blog.html and subsequent blog pages, and added svg transition to burger nav in nav.css and burger.js

--- javascript/burger.js
+++ javascript/burger.js
@@ -1,6 +1,8 @@
 const nav = document.getElementById('navBurger');
-const burger = document.getElementById('burger');
+const burger = document.getElementById('burgerButton');
 
    burger.addEventListener('click', () => {
+      burger.classList.toggle('opened');
+      burger.setAttribute('aria-expanded', nav.classList.contains('nav-ul'))
       nav.classList.toggle('nav-ul');
    });
