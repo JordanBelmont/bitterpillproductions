0cca55e4a253f7968685f844a816db9091e34b10
Jordan Belmont <jordan.belmont@hotmail.com>
2020-12-18 14:19:06 +0000

    Added media queries for larger desktop sizes

--- javascript/lightbox.js
+++ javascript/lightbox.js
@@ -3,10 +3,10 @@
 let lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
-
-// LIGHTBOX ON CLICK
-
 let images = document.querySelectorAll('.gallery-image')
+
+// LIGHTBOX ON MOUSE-CLICK
+
 images.forEach(image => {
    image.addEventListener('click', () => {
       lightbox.classList.add('active')
@@ -19,16 +19,33 @@ images.forEach(image => {
    })
 });
 
-// LIGHTBOX OFF CLICK
+// LIGHTBOX ON ENTER
+
+images.forEach(image => {
+   image.addEventListener('keyup', e => {
+      if (e.key === "Enter") {
+         lightbox.classList.add('active')
+         let img = document.createElement('img')
+         img.src = image.src
+         while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+         }
+         lightbox.appendChild(img)
+      }}
+)});
+
+// LIGHTBOX OFF MOUSE-CLICK
 
 lightbox.addEventListener('click', e => {
-   if (e.target !== e.currentTarget) 
-   return
-   lightbox.classList.remove('active')
+   if (e.target !== e.currentTarget && e.target === e.currentTarget) 
+   return;
+   lightbox.classList.remove('active');
 });
 
-document.addEventListener('keyup', e => { 
-   if (e.key == 27)
-   return
-   lightbox.classList.remove('active');
+// LIGHTBOX OFF ESC
+
+window.addEventListener('keyup', e => { 
+   if (e.key === 'Escape') { 
+      lightbox.classList.remove('active');
+   }
 });
